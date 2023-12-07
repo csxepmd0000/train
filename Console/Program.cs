1cb4114d8c0001bb1e08a9dab5b6c6af3bb5412c
陳 世雄 <chen@uchida-it.co.jp>
2023-12-07 22:18:00 +0900

    end init

--- Console/Program.cs
+++ Console/Program.cs
@@ -4,12 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 1; i <= 100; i++)
-            {
-                Console.WriteLine(FizzBuzzC.CheckFizzBuzz(i));
-            }
+        Console.Write("数字を入力してください:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= number; i++)
+        {
+            Console.WriteLine(FizzBuzzC.CheckFizzBuzz(i));
+        }
 
-            Console.WriteLine("Press enter to quit...");
-            Console.ReadLine();
+        Console.WriteLine("Press enter to quit...");
+        Console.ReadLine();
     }
 }
