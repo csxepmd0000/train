25940ddb17ac099577a9242efd54798b057b9978
陳 世雄 <chen@uchida-it.co.jp>
2023-12-07 21:56:12 +0900

    init fizzbuzz

--- Console/Program.cs
+++ Console/Program.cs
@@ -4,34 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        int row = 0;
-
-        do
-        {
-            if (row == 0 || row >= 25)
-                ResetConsole();
-
-            string? input = Console.ReadLine();
-            if (string.IsNullOrEmpty(input)) break;
-            Console.WriteLine($"Input: {input}");
-            Console.WriteLine("Begins with uppercase? " +
-                 $"{(input.StartsWithUpper() ? "Yes" : "No")}");
-            Console.WriteLine();
-            row += 4;
-        } while (true);
-        return;
-
-        // Declare a ResetConsole local method
-        void ResetConsole()
-        {
-            if (row > 0)
+        for (int i = 1; i <= 100; i++)
             {
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
+                Console.WriteLine(FizzBuzzC.GetOutput(i));
             }
-            Console.Clear();
-            Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
-            row = 3;
-        }
+
+            Console.WriteLine("Press enter to quit...");
+            Console.ReadLine();
     }
 }
