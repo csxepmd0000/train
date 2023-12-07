634e59062e6a5616759be9213e87e466dc78c365
陳 世雄 <chen@uchida-it.co.jp>
2023-12-07 20:30:27 +0900

    new sln

---
+++ FizzBuzz/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzzNS;
+
+public static class FizzBuzzC
+{
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+}
