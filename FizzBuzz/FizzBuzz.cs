e2169e791c42fd120a7c0d2194dacd6f243e2334
陳 世雄 <chen@uchida-it.co.jp>
2023-12-15 14:16:54 +0900

    ignore4

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzzNS;
+
+public static class FizzBuzzC
+{
+    public static string CheckFizzBuzz(int number)
+    {
+        string output;
+
+        if (number > 100)
+        {
+            output = number.ToString("error");
+            return output;
+        }
+
+        if ((number % 3 == 0) && (number % 5 == 0))
+        {
+            output = "FizzBuzz";
+        }
+        else if (number % 3 == 0)
+        {
+            output = "Fizz";
+        }
+        else if (number % 5 == 0)
+        {
+            output = "Buzz";
+        }
+        else
+        {
+            output = number.ToString();
+        }
+
+
+        return output;
+    }
+}
