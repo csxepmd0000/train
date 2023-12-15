7b1a567f9ec8dba48c1bb51c619512f0b519b375
陳 世雄 <chen@uchida-it.co.jp>
2023-12-15 14:13:43 +0900

    cache delete2

---
+++ Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using FizzBuzzNS;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数字を入力してください:");
+        string strNumber = Console.ReadLine();
+        if (int.TryParse(strNumber, out int number2))
+        {
+            for (int i = 1; i <= Convert.ToInt32(strNumber); i++)
+            {
+                Console.WriteLine(FizzBuzzC.CheckFizzBuzz(i));
+            }
+
+            Console.WriteLine("Press enter to quit...");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.Write("error");
+        }
+    }
+}
